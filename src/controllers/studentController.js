0745e4413c52ce3d00be8f8bbfd02502145c8adf
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-02-08 18:52:28 +0530

    update

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const db = require("../config/db");
 
 exports.getAllStudents = (req, res) => {
@@ -6,5 +7,11 @@ exports.getAllStudents = (req, res) => {
     res.json(results);
   });
 
-  
+  exports.createStudent = (req, res) => {
+    const { name, age, grade } = req.body;  
+    db.query("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", [name, age, grade], (err, results) => {
+      if (err) return res.status(500).json(err);
+      res.status(201).json({ id: results.insertId, name, age, grade });
+    });
+  };
 };
