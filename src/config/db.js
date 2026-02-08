911f69b15c387b0a1ab478a7610f31cea2ce963b
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-02-08 19:32:25 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Ulindul@123",
+  password: "Ulindu@123",
   database: "node_express_studentmanagement"
 });
 
